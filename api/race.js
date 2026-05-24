109b4d99ec310266ef90c7b46d94d2c8f39a1f05
bigi0616 <116911416+bigi0616@users.noreply.github.com>
2026-05-25 00:08:46 +0900

    Update race.js

--- api/race.js
+++ api/race.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
   const { serviceKey, meet, rc_date, rc_no } = req.query;
 
   try {
-    const url = `https://apis.data.go.kr/B551015/API186_1/RcRaceInfo_1?serviceKey=${serviceKey}&numOfRows=20&pageNo=1&rc_date_fr=${rc_date}&rc_date_to=${rc_date}&_type=json`;
+    const url = `https://apis.data.go.kr/B551015/API186_1/SeoulRace_1?serviceKey=${serviceKey}&numOfRows=20&pageNo=1&rc_date_fr=${rc_date}&rc_date_to=${rc_date}&_type=json`;
     const response = await fetch(url);
     const text = await response.text();
     return res.status(200).send(text);
