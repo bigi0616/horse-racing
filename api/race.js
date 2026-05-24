d3532269a4b194aeb2e72cd16e737adee2df26ab
bigi0616 <116911416+bigi0616@users.noreply.github.com>
2026-05-25 00:49:12 +0900

    Update race.js

--- api/race.js
+++ api/race.js
@@ -1,19 +1,15 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
+  if (req.method === 'OPTIONS') return res.status(200).end();
 
   const { serviceKey, meet, rc_date, rc_no } = req.query;
 
   try {
-    const url = `https://apis.data.go.kr/B551015/API186_1/SeoulRace_1?serviceKey=${serviceKey}&numOfRows=20&pageNo=1&rc_date_fr=${rc_date}&rc_date_to=${rc_date}&_type=json`;
+    const url = `https://apis.data.go.kr/B551015/API4_3/raceResult_3?serviceKey=${serviceKey}&numOfRows=20&pageNo=1&meet=${meet}&rc_date=${rc_date}&rc_no=${rc_no}&_type=json`;
     const response = await fetch(url);
     const text = await response.text();
     return res.status(200).send(text);
-    
   } catch (e) {
     return res.status(500).json({ error: e.message });
   }
