6c88d601ab4f72c89ba261d83424d4b96d60d673
bigi0616 <116911416+bigi0616@users.noreply.github.com>
2026-05-25 18:06:47 +0900

    Update race.js

--- api/race.js
+++ api/race.js
@@ -7,12 +7,15 @@ export default async function handler(req, res) {
 
   try {
     let url;
-    
+
     if (type === 'result') {
-      // 경주성적정보 (과거 성적)
+      // 경주성적정보
       url = `https://apis.data.go.kr/B551015/API214_1/RaceDetailResult_1?serviceKey=${serviceKey}&numOfRows=20&pageNo=1&meet=${meet}&rc_date=${rc_date}&rc_no=${rc_no}&_type=json`;
+    } else if (type === 'stats') {
+      // 최근 한달 성적 통계용 (rc_no 없이 날짜 범위로 조회)
+      url = `https://apis.data.go.kr/B551015/API214_1/RaceDetailResult_1?serviceKey=${serviceKey}&numOfRows=100&pageNo=1&meet=${meet}&rc_date=${rc_date}&_type=json`;
     } else {
-      // 경주기록정보 (출전마 정보) - 기본
+      // 경주기록정보 기본
       url = `https://apis.data.go.kr/B551015/API4_3/raceResult_3?serviceKey=${serviceKey}&numOfRows=20&pageNo=1&meet=${meet}&rc_date=${rc_date}&rc_no=${rc_no}&_type=json`;
     }
 
